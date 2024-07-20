334c588429f11b77b6bec582b4e296fe69137708
shubham kumar <shubhofficial1@gmail.com>
2024-07-20 17:49:09 +0530

    Added Skills Section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import Banner from "./components/Banner";
 import Hero from "./components/Hero";
-import About from "./components/About";
 import Footer from "./components/ui/Footer";
+import Skills from "./components/Skills";
 const App = () => {
   return (
     <>
       <Banner />
       <Hero />
-      <About />
+      <Skills />
       <Footer />
     </>
   );
