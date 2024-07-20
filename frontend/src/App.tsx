593210326661c8055b07cb62276d791028840753
shubham kumar <shubhofficial1@gmail.com>
2024-07-20 16:28:33 +0530

    Added Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import Banner from "./components/Banner";
 import Hero from "./components/Hero";
 import About from "./components/About";
-
+import Footer from "./components/ui/Footer";
 const App = () => {
   return (
     <>
       <Banner />
       <Hero />
       <About />
+      <Footer />
     </>
   );
 };
